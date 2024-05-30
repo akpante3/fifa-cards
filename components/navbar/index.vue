4d29ad521fd2400560a1256ca56acd606cbbbd0c
Victor <career.victorobije@gmail.com>
2024-05-30 04:37:38 +0100

    NavBar

--- components/navbar/index.vue
+++ components/navbar/index.vue
@@ -2,8 +2,8 @@
 <template>
     <nav class="bg-gray-800 p-4">
       <ul class="flex justify-between">
-        <li><img src="~/asset/icons/menu.svg" alt="My Icon" class="w-6 h-6" /></li>
-        <li><NuxtLink to="/contact" class="text-white hover:text-gray-300">Contact</NuxtLink></li>
+        <li><img src="../../assets/icons/menu.svg" alt="My Icon" class="w-6 h-6" /></li>
+        <li><NuxtLink to="/contact" class="text-white hover:text-gray-300"><img src="../../assets/icons/RS-logo.png" alt="My Icon" class="w-15 h-6" /></NuxtLink></li>
       </ul>
     </nav>
   </template>
